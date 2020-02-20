e10c571ea451785738baac0e2d5bcc32564bfc99
Rodrigo da Silva Santos <rodrigo.santos208@fatec.sp.gov.br>
2020-02-20 00:13:23 -0300

    Testa o link de cadastro

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -1,8 +1,13 @@
 require 'rails_helper'
 
 feature "Welcome", type: :feature do
- scenario 'Shows Welcome message' do
-  visit('/')
-  expect(page).to have_content('Bem-vindo')
- end
+ 	scenario 'Shows Welcome message' do
+		visit(root_path)
+		expect(page).to have_content('Bem-vindo')
+	end
+
+	scenario 'Verify customer base link'do
+    visit(root_path)
+		expect(find('ul li')).to have_link('Cadastro de Clientes')
+	end
 end
